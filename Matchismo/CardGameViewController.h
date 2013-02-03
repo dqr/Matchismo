629f6246b238ef799163e94b1ff4152d302dbdad
David Rolin <dwr@Davids-iMac.local>
2013-02-03 14:04:19 -0500

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by David Rolin on 2/3/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ MatchismoTests/MatchismoTests.h
@@ -0,0 +1,13 @@
+//
+//  MatchismoTests.h
+//  MatchismoTests
+//
+//  Created by David Rolin on 2/3/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MatchismoTests : SenTestCase
+
+@end
