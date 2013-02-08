3aa0ce13cde6198b27a82656d068acef6ea46923
Dave Rolin <dwr@Davids-iMac.local>
2013-02-08 16:12:35 -0500

    Lecture 3 slides 5 - 43.  Increased to 12 buttons and slight refactor in CardGameViewController.m to match slides.

--- Matchismo Notes.md
+++ Matchismo Notes.md
@@ -28,8 +28,15 @@ Now to the beginning part, the UI...
 
 Looking at the commit log and the state of the product, it looks like we left off with a working Homework 0, which is the last thing in the Lecture 2 Slides.
 
-On to Lecture 3…
+Added this file (these notes) to the repository
 
+On to Lecture 3…  Looks like we'll start at slide 5 and just work linearly through the slides doing the stuff.  Once that's done, we'll be on to Assignment 1 as alluded to on slide 129.
+
+Slides 5 - 33 just deal with going from 1 button to 12 of them.
+
+Slides 34 - 43 deal with what was done in HW 0, i.e. making a random deck, not just the ace of clubs.  On slide 34 Paul uses a single line if (no braces).  I think I'll keep the braces, but shrink it to one line too.  Throw a commit in here for good measure.
+
+Slides xxx
 
 
 
--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -13,10 +13,9 @@
 @interface CardGameViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
-
 @property (nonatomic) int flipCount;
-
 @property (strong, nonatomic) Deck *deck;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 
 @end
 
@@ -25,11 +24,18 @@
 // use lazy instantiation for the deck getter
 - (Deck *)deck
 {
-    if (!_deck)
-    {
-        _deck = [[PlayingCardDeck alloc] init];
-    }
+    if (!_deck) { _deck = [[PlayingCardDeck alloc] init]; }
     return _deck;
+}
+
+- (void) setCardButtons:(NSArray *)cardButtons
+{
+    _cardButtons = cardButtons;
+    for (UIButton *cardButton in cardButtons)
+    {
+        Card *card = [self.deck drawRandomCard];
+        [cardButton setTitle:card.contents forState:UIControlStateSelected];
+    }
 }
 
 - (void) setFlipCount:(int)flipCount
@@ -41,11 +47,6 @@
 - (IBAction)flipCard:(UIButton *)sender
 {
     sender.selected = !sender.isSelected;
-    Card *card = [self.deck drawRandomCard];
-    // if we went face up (isSelected), then
-    // get a random card and set our button's label
-    // text from contents
-    [sender setTitle:card.contents forState:UIControlStateSelected];
     self.flipCount++;
 }
 
