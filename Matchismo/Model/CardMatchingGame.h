6fae71808f56047a0681f6c9d39810f289910725
Dave Rolin <dwr@Davids-iMac.local>
2013-02-09 16:46:11 -0500

    Implement stuff on lecture 3 slides up to slide 79.

---
+++ Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,24 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Dave Rolin on 2/9/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface CardMatchingGame : NSObject
+
+// This is our designated initializer
+- (id)initWithCardCount:(NSInteger)cardCount
+              usingDeck:(Deck *)deck;
+
+- (void)flipCardAtIndex:(NSInteger)index;
+
+- (Card *)cardAtIndex:(NSInteger)index;
+
+@property (nonatomic, readonly) int score;
+
+@end
---
+++ Matchismo/Model/CardMatchingGame.m
@@ -0,0 +1,88 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by Dave Rolin on 2/9/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@interface CardMatchingGame()
+@property (strong, nonatomic) NSMutableArray *cards;
+@property (nonatomic) int score;
+@end
+
+@implementation CardMatchingGame
+
+- (NSMutableArray *)cards
+{
+    if (!_cards) { _cards = [[NSMutableArray alloc]init]; }
+    return _cards;
+}
+
+- (id)initWithCardCount:(NSInteger)count
+              usingDeck:(Deck *)deck
+{
+    self = [super init];
+    if (self)
+    {
+        for (int i = 0; int < count; i++)
+        {
+            Card *card = [deck drawRandomCard];
+            if (!card)
+            {
+                self = nil;
+            }
+            else
+            {
+                self.cards[i] = card;
+            }
+        }
+    }
+
+    return self;
+
+}
+
+- (Card *)cardAtIndex:(NSInteger)index
+{
+    return (index < self.cards.count) ? self.cards[index] : nil;
+}
+
+#define flipCost 1
+#define MATCH_BONUS 4
+#define MISMATCH_PENALTY 2
+
+- (void)flipCardAtIndex:(NSInteger)index
+{
+    Card *card = [self cardAtIndex:index];
+    if (!card.isUnplayable)
+    {
+        if (!card.isFaceUp)
+        {
+            for (Card *otherCard in self.cards)
+            {
+                if (otherCard.isFaceUp && !otherCard.isUnplayable)
+                {
+                    int matchScore = [card match:@[otherCard]];
+                    if (matchScore)
+                    {
+                        otherCard.unplayable = YES;
+                        card.unplayable = YES;
+                        self.score += matchScore*MATCH_BONUS;
+                    }
+                    else
+                    {
+                        otherCard.faceUp = NO;
+                        self.score -= MISMATCH_PENALTY;
+                    }
+                }
+            }
+            self.score -= flipCost;
+        }
+        card.faceUp = !card.isFaceUp;
+    }
+}
+
+@end
